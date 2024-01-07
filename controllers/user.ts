3cae6c93f2f8e3e08f31b50b4d0de3dc4eae3ebd
kaytee07 <kofi.taylor07@gmail.com>
2024-01-07 07:35:04 +0000

    endpoint for getUser, deleteUser, getallusers and number of users dunctional

--- controllers/user.ts
+++ controllers/user.ts
@@ -27,7 +27,17 @@ class UserController {
 
         if (error) res.status(400).json({"error": error});
 
-        const { firstName, lastName, email, username, password} = value;
+        const { firstName, lastName, email, password} = value;
+
+        try {
+             const EmailInDB = await UserModel.findOne({email});
+             if (EmailInDB) {
+                return res.status(400).json({"error": "Email already exist"});
+             }
+        } catch(err) {
+            res.status(400).json({"error": err})
+        }
+       
         const hashedPassword = await UserController.hashPassword(password);
         const user = new UserModel({
             firstName,
@@ -43,6 +53,47 @@ class UserController {
         }).catch((error) => {
             console.error(error);
         })
+    }
+
+    static async getAllUsers(req: Request, res:Response) {
+        try {
+            const numOfUsers = await UserModel.find()
+            return res.status(200).json({"number_of_users": numOfUsers})
+        } catch (err) {
+            res.status(400).json({"Error": err})
+        }
+        
+    }
+
+    static async deleteUser(req: Request, res: Response) {
+        const { email } = req.body;
+        try {
+            const user = await UserModel.deleteOne({email});
+            console.log(user);
+            return res.status(200).json({"sucessful": `deleted ${user}`})
+        } catch (err) {
+            return res.status(400).json({"Error": err});
+        }
+        
+    }
+
+    static async getUser(req: Request, res: Response) {
+        const { email } = req.body;
+        try {
+            const user = await UserModel.findOne({email});
+            return res.status(200).json({"sucessful": `${user}`})
+        } catch (err) {
+            return res.status(400).json({"Error": err});
+        }
+    }
+
+    static async getNumberOfUsers(req: Request, res: Response) {
+        try {
+            const numOfUsers = await UserModel.countDocuments({});
+            return res.status(200).json({"sucessful": numOfUsers});
+        } catch (err) {
+            return res.status(400).json({"Error": err});
+        }
     }
 
 }
--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -4,5 +4,9 @@ import { UserController } from '../controllers';
 const UserRouter = express.Router();
 
 UserRouter.post("/signup", UserController.createUser);
+UserRouter.get("/allusers", UserController.getAllUsers);
+UserRouter.get("/user", UserController.getUser);
+UserRouter.post("/removeuser", UserController.deleteUser);
+UserRouter.get("/numberofusers", UserController.getNumberOfUsers);
 
 export default UserRouter;
